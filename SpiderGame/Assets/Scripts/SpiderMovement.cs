d2157b4acd0401e40ad9995397ed8656274849c3
Benjamin Gillott <bg357@cornell.edu>
2021-01-22 18:43:49 -0500

    prog

--- SpiderGame/Assets/Scripts/SpiderMovement.cs
+++ SpiderGame/Assets/Scripts/SpiderMovement.cs
@@ -4,37 +4,46 @@ using UnityEngine;
 
 public class SpiderMovement : MonoBehaviour
 {
-    public float startX;
-    public float startY;
 
+    public float patrolY;
+    public float patrolMinX;
+    public float patrolMaxX;
+    public float patrolStartX;
+    public float patrolSpeed;
+    public float chaseSpeed;
+    public float returnSpeed;
+    public float killDistance;
+    
+    public Transform player;
 
+    private float distance; //Start default
+    private string state;
 
+    // States:
+    // Patrol - change x pos on the line by patrolSpeed
+    // Chase - move chaseSpeed towards player
+    // Return - move y up to nearest track point by returnSpeed
+    // Kill - trigger kill event (TBD) - not a full state but a result
 
-    //TODO: Switch to bounding box or cinemachine track for the sine math
-    public float trackMinX; 
-    public float trackMaxX;
-    public float moveSpeed;
-    private bool headingRight;
+    // Transitions:
+    // Patrol -> Chase : if (distance < hearable distance) && $$playerVelocity > 0
+    // Chase -> Kill : If dist < killDist
+    // Chase -> Return : if player velocity <= 0 (add slight delay?)
+    // Return -> Patrol : if spiderY ~= patrolY
+    // Return -> Chase : (same as Patrol -> Chase)
 
+    
     // Start is called before the first frame update
     void Start()
     {
-        //this.transform.position = new Vector3(startX, startY, this.transform.position.z);
+        distance = 1000;//euclidian dist
+        state = "Patrol";
+        this.transform.position = new Vector3(patrolMaxX, patrolY, this.transform.position.z);
+        
     }
 
     void Update()
     {
-        // //Move on a linear track
-        // float curX = this.transform.position.x;
-
-        // if (headingRight){
-        //     curX += moveSpeed * Time.deltaTime;
-        //     if (curX > trackMaxX){headingRight = false;}
-        // }else{
-        //     curX -= moveSpeed;
-        //     if (curX < trackMinX){headingRight = true;}
-        // }
-
         // this.transform.position = new Vector3(curX, this.transform.position.y, this.transform.position.z);
     }
 }
