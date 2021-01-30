5e336437f4f569fd0bec40b864a3b5c1db36de90
Benjamin Gillott <bg357@cornell.edu>
2021-01-30 13:38:17 -0500

    footsteps be poppin' off

---
+++ SpiderGame/Assets/Scripts/FootstepEventCall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepEventCall : MonoBehaviour
+{
+    // public AudioSource footstep_source;
+    public AudioClip[] footstepsArray;
+    public AudioSource audioSource;
+
+    public void PlayFootstep () {
+        // footstep_source.Play();
+        AudioClip clip = (AudioClip)footstepsArray[Random.Range(0, footstepsArray.Length)];
+        audioSource.PlayOneShot(clip, 1.0f);
+    }
+
+}
